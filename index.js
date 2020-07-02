c80c12fbbcb5acaef67ef14f696abaed59cd3017
Amogha <saanvi@UshaLaptop.calient.local>
2020-07-02 15:32:19 -0700

    javascript added

--- index.js
+++ index.js
@@ -5,3 +5,8 @@ function saySomething(variable) {
 function changeColor(color) {
     document.getElementById("para").style
 }
+<script>
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";
+}
+</script>
